57d109b0b6dc7e7780801eb29d66e0b31099d48e
Simon Jefford <simon.jefford@gmail.com>
2012-08-31 17:16:07 +0100

    Tidy up

--- src/reflookup/core.clj
+++ src/reflookup/core.clj
@@ -2,7 +2,6 @@
   (:use [clojurewerkz.urly.core :only (host-of query-of)]
         [clojure.string :only [split join]]))
 
-
 (defn add-referral [url params_lookup]
   (let [host (host-of url)
         additional (map (fn [kvp] (join "=" kvp)) (params_lookup host))]
