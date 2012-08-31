285dccc305b9b5e52767fbb8740333df6d005fce
Simon Jefford <simon.jefford@gmail.com>
2012-08-31 18:02:36 +0100

    test for multiple pieces of ref info

--- test/reflookup/core_test.clj
+++ test/reflookup/core_test.clj
@@ -10,6 +10,13 @@
            (add-referral "http://example.com" google_lookup)))))
 
 (deftest addreferral_with_existing_query_string
-  (testing "adds a single piece of referral information to a 'simple' url"
+  (testing "adds a single piece of referral information to a url with a query string"
     (is (= "http://example.com/?referrer_id=1&foo=1"
            (add-referral "http://example.com/?foo=1" google_lookup)))))
+
+(def another_lookup { "example.com" {"referrer_id" 1 "bar" 42}})
+
+(deftest addreferral_with_existing_query_string
+  (testing "adds a single piece of referral information to a url with a query string"
+    (is (= "http://example.com/?bar=42&referrer_id=1&foo=1"
+           (add-referral "http://example.com/?foo=1" another_lookup)))))
