4bacd5e5459ae25de6e64931ab9bdbea9454d476
Simon Jefford <simon.jefford@gmail.com>
2012-08-31 17:59:41 +0100

    Small refactor

--- src/reflookup/core.clj
+++ src/reflookup/core.clj
@@ -2,12 +2,15 @@
   (:use [clojurewerkz.urly.core :only (host-of query-of url-like)]
         [clojure.string :only [split join]]))
 
+(defn strip-nils [seq]
+  (filter identity seq))
+
 (defn add-referral [url params_lookup]
   (let [url (url-like url)
         host (host-of url)
         query [(query-of url)]
         additional (map (fn [kvp] (join "=" kvp)) (params_lookup host))]
-    (str (.withoutQuery url) "?" (join "&" (filter identity (concat additional query))))))
+    (str (.withoutQuery url) "?" (join "&" (strip-nils (concat additional query))))))
 
 (comment
 
