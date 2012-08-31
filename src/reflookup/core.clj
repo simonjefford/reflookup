e7976b6017375ad891ea8f504490df9afba615bb
Simon Jefford <simon.jefford@gmail.com>
2012-08-31 17:33:52 +0100

    Cope with URLs with existing query strings

--- src/reflookup/core.clj
+++ src/reflookup/core.clj
@@ -1,8 +1,18 @@
 (ns reflookup.core
-  (:use [clojurewerkz.urly.core :only (host-of query-of)]
+  (:use [clojurewerkz.urly.core :only (host-of query-of url-like)]
         [clojure.string :only [split join]]))
 
 (defn add-referral [url params_lookup]
-  (let [host (host-of url)
+  (let [url (url-like url)
+        host (host-of url)
+        query [(query-of url)]
         additional (map (fn [kvp] (join "=" kvp)) (params_lookup host))]
-    (str url "?" (join "&" additional))))
+    (str (.withoutQuery url) "?" (join "&" (filter identity (concat additional query))))))
+
+(comment
+
+(def google_lookup { "example.com" {"referrer_id" 1}})
+(add-referral "http://example.com?foo=1" google_lookup)
+(add-referral "http://example.com" google_lookup)
+
+)
--- test/reflookup/core_test.clj
+++ test/reflookup/core_test.clj
@@ -6,5 +6,10 @@
 
 (deftest addreferral_simple
   (testing "adds a single piece of referral information to a 'simple' url"
-    (is (= "http://example.com?referrer_id=1"
+    (is (= "http://example.com/?referrer_id=1"
            (add-referral "http://example.com" google_lookup)))))
+
+(deftest addreferral_with_existing_query_string
+  (testing "adds a single piece of referral information to a 'simple' url"
+    (is (= "http://example.com/?referrer_id=1&foo=1"
+           (add-referral "http://example.com/?foo=1" google_lookup)))))
